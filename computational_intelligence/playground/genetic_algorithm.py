8fcf317987a80713c265ea13753de5eb11a8936c
Ernest Roszak <74867891+roszerne@users.noreply.github.com>
2023-05-09 11:32:32 +0200

    Generating reports in PDF (#1)
    * age_playground.py - used for testing average age of solutions, age_classes.py - stores classess necessary for age_playgrond
    
    * 1. Obsever added to socjo_SSGA. 2. Clean code and fixing age_playground
    
    * 1. Added 3 weights for computing probablity of exchange: BASIC_PROB, TRUST_PROB, COST_PROB
    2. socjo_SSGA is now using classes from age_classes.py, especially MyRastrigin instead of Sphere
    3. Added PrintObjectivesObserver
    
    * format
    
    * 1. Solutions are now evaluated after exchange
    2. Genes to exchange are randomized
    3. Graphs are plotted: a) comparison between average and best fitness for one group parmaters b) comparison of best fitness between all groups
    c) comparison of average fitness between all groups
    
    * format
    
    * clean
    
    * implement real mutation and better crossover
    
    * optimize evaluations
    
    * add real mutation
    
    * change implementation to more `standard`
    
    * change parameters
    
    * format
    
    * clean the code
    
    * PDF are created for every problem (only for socjo_SSGA.py class)
    
    * Saving reports for socjo and genetic algorithms.
    
    * Delete computational_intelligence/playground/__pycache__ directory
    
    * Delete jmetalpy.log
    
    * Comments deleted
    
    ---------
    
    Co-authored-by: war-in <marcin.warchol20@gmail.com>

--- computational_intelligence/playground/genetic_algorithm.py
+++ computational_intelligence/playground/genetic_algorithm.py
@@ -1,23 +1,17 @@
 import matplotlib.pyplot as plt
+import os
+from datetime import datetime
+from matplotlib.backends.backend_pdf import PdfPages
 from jmetal.algorithm.singleobjective.genetic_algorithm import GeneticAlgorithm
 from jmetal.core.observer import Observer
-from jmetal.core.operator import Crossover
-from jmetal.core.solution import BinarySolution
-from jmetal.lab.visualization import Plot
-from jmetal.operator import (
-    BinaryTournamentSelection,
-    BitFlipMutation,
-    PolynomialMutation,
-    SimpleRandomMutation,
-    UniformMutation,
-)
-from jmetal.operator.crossover import CXCrossover, PMXCrossover, SBXCrossover
-from jmetal.operator.selection import BestSolutionSelection, RouletteWheelSelection
-from jmetal.problem import ZDT1
-from jmetal.problem.singleobjective.unconstrained import OneMax, Rastrigin, Sphere
+from jmetal.operator import UniformMutation
+from jmetal.operator.crossover import SBXCrossover
+from jmetal.operator.selection import RouletteWheelSelection
+from jmetal.problem.singleobjective.unconstrained import Rastrigin, Sphere
 from jmetal.util.observer import LOGGER
-from jmetal.util.solution import get_non_dominated_solutions
 from jmetal.util.termination_criterion import StoppingByEvaluations
+
+from socjo_SSGA import SocioSSGA
 
 
 class PrintObjectivesObserver(Observer):
@@ -33,50 +27,137 @@ class PrintObjectivesObserver(Observer):
         evaluations = kwargs["EVALUATIONS"]
         solutions = kwargs["SOLUTIONS"]
 
-        if (evaluations % self.display_frequency) == 0 and solutions:
+        if evaluations % self.display_frequency == 0 and solutions:
             if isinstance(solutions, list):
-                fitness = solutions[0].objectives
+                fitness = sorted(solutions, key=lambda solution: solution.objectives[0])
             else:
-                fitness = solutions.objectives
+                fitness = solutions.objectives[0]
 
             self.epoch.append(evaluations)
             self.fitness.append(fitness)
 
-            LOGGER.info("Evaluations: {}. fitness: {}".format(evaluations, fitness))
+            LOGGER.info(f"Evaluations: {evaluations}. fitness: {fitness}")
 
+
+def save_to_pdf(problems, plots_per_problem, note):
+
+    dir_name = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
+    os.mkdir(str(dir_name))
+
+    fig_nums = plt.get_fignums()  
+    figs = [plt.figure(n) for n in fig_nums]
+
+    for i in range (0, len(problems)): 
+
+        
+        filename =  str(dir_name) + "/" + problems[i].get_name() + str(problems[i].number_of_variables) + ".pdf"
+        p = PdfPages(filename)
+        p.attach_note(note, [0,0])
+        for j in range(0, plots_per_problem):
+
+            fig = figs[i * plots_per_problem + j]
+            fig.savefig(p, format='pdf') 
+
+        
+        p.close()  
 
 if __name__ == "__main__":
-    problem = Sphere(50)
 
-    mutation_algorithms = [
-        PolynomialMutation(probability=0.1),
-        SimpleRandomMutation(probability=0.1),
-        UniformMutation(probability=0.1),
-    ]
+    basic_probs = [0.1, 0.2, 0.3, 0.1]
+    trust_probs = [0.6, 0.7, 0.6, 0.5]
+    cost_probs = [0.3, 0.1, 0.1, 0.4]
+
+    population_size = 100
+    offspring_population_size = 1
+    interaction_probability = 0.5
+    mutation_probability = 0.1
+    crossover_probability = 0.9
+    evaluations = 20000
+    observer_freq = 10
+
+    #basic_probs = [0.1]
+    #trust_probs = [0.6]
+    #cost_probs = [0.3]
+
+    sizes = [50, 100, 200]
+    problems = []
+
+    for size in sizes:
+        problems.append(Rastrigin(size))
+        problems.append(Sphere(size))
+
+    plots_per_problem = 1 # how many charts should be printed for each problem
+    number_of_trials = 1  # number of tests per problem 
+
+    epoch = []
     fitness = []
+    average_fitness = []
 
-    for mutation_algorithm in mutation_algorithms:
-        algorithm = GeneticAlgorithm(
-            problem=problem,
-            population_size=10,
-            offspring_population_size=1,
-            mutation=mutation_algorithm,
-            crossover=SBXCrossover(probability=0.9),
-            selection=BestSolutionSelection(),
-            termination_criterion=StoppingByEvaluations(10000),
-        )
+    test_data = ("Population size: " + str(population_size) + "\n" +
+    "Offspring size: " + str(offspring_population_size) + "\n" +
+    "Interaction probability: " + str(interaction_probability) + "\n" +
+    "Mutation probability: " + str(mutation_probability) + "\n" +
+    "Crossover probability: " + str(crossover_probability) + "\n" +
+     "Evaluations: " + str(evaluations) )
 
-        observer = PrintObjectivesObserver(1)
-        algorithm.observable.register(observer)
+    for problem in problems:
 
-        algorithm.run()
+        algorithms = [
+            GeneticAlgorithm(
+                problem=problem,
+                population_size=population_size,
+                offspring_population_size=population_size // 2,
+                mutation=UniformMutation(probability=0.1),
+                crossover=SBXCrossover(probability=0.9),
+                selection=RouletteWheelSelection(),
+                termination_criterion=StoppingByEvaluations(evaluations),
+            ),
+            SocioSSGA(
+                problem=problem,
+                population_size=population_size,
+                offspring_population_size=1,
+                interaction_probability=0.5,
+                selection=RouletteWheelSelection(),
+                mutation=UniformMutation(probability=0.1),
+                crossover=SBXCrossover(probability=0.9),
+                basic_prob=0.2,
+                trust_prob=0.7,
+                cost_prob=0.1,
+                max_switched_genes=int(problem.number_of_variables * 0.75),
+                termination_criterion=StoppingByEvaluations(evaluations),
+            ),
+        ]
 
-        fitness.append(observer.fitness)
+        epoch = []
+        fitness = []
+        average_fitness = []
+        
+        for algorithm in algorithms:
 
-    plt.xlabel("Ewaluacje")
-    plt.ylabel("Fitness")
-    plt.title("Porównanie różnych operatorów mutacji")
-    for i, data in enumerate(zip(fitness, mutation_algorithms)):
-        plt.plot(data[0], label=data[1].get_name())
-    plt.legend()
-    plt.show()
+
+            #epoch = [0] * 5
+            #fitness = [0] * 5
+            #average_fitness = [0] * 5
+
+            #for j in range(0, number_of_trials):
+
+            observer = PrintObjectivesObserver(observer_freq)
+            algorithm.observable.register(observer)
+
+            algorithm.run()
+
+            epoch.append(observer.epoch)
+            fitness.append(observer.fitness)
+            #average_fitness.append(observer.average_fitness)
+
+        plt.figure()
+        plt.xlabel("Ewaluacje")
+        plt.ylabel("Fitness")
+        plt.title("Porównanie różnych operatorów mutacji")
+        for i, data in enumerate(zip(epoch, fitness, algorithms)):
+            plt.plot(data[0], data[1], label=data[2].get_name())
+        plt.legend()
+        #plt.show()
+
+    save_to_pdf(problems, plots_per_problem, test_data)
--- computational_intelligence/playground/socjo_SSGA.py
+++ computational_intelligence/playground/socjo_SSGA.py
@@ -1,13 +1,25 @@
 import random
+import numpy as np
+import os
+from datetime import datetime
+from matplotlib.backends.backend_pdf import PdfPages
+from copy import copy
 from typing import Dict, List
 
+import matplotlib.pyplot as plt
 from jmetal.config import store
 from jmetal.core.algorithm import EvolutionaryAlgorithm, R, S
+from jmetal.core.observer import Observer
+from jmetal.core.operator import Crossover, Mutation, Selection
 from jmetal.core.problem import Problem
 from jmetal.core.solution import FloatSolution
-from jmetal.problem import Sphere
+from jmetal.operator import UniformMutation
+from jmetal.operator.crossover import SBXCrossover
+from jmetal.operator.selection import RouletteWheelSelection
+from jmetal.problem.singleobjective.unconstrained import Rastrigin, Sphere
 from jmetal.util.evaluator import Evaluator
 from jmetal.util.generator import Generator
+from jmetal.util.observer import LOGGER
 from jmetal.util.termination_criterion import (
     StoppingByEvaluations,
     TerminationCriterion,
@@ -19,11 +31,17 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
     Socio-cognitive steady state genetic algorithm.
     """
 
-    ranking: Dict[S, int]
+    ranking: Dict[int, int] = {}
     """
     Current ranking for solutions.
-    ranking[solution] - "trust" for solution
+    ranking[solution.id] - "trust" for solution
     """
+    MAX_TRUST: int = 100
+    MIN_TRUST: int = 0
+    BASIC_PROB: float  # Minimal probability that the exchange will occur
+    TRUST_PROB: float  # Weight of probability gained from trust ranking
+    COST_PROB: float  # Weight of probability gained from better evaluation
+    MAX_SWITCHED_GENES: int
 
     def __init__(
         self,
@@ -31,13 +49,32 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
         population_size: int,
         offspring_population_size: int,
         interaction_probability: float,
+        selection: Selection,
+        mutation: Mutation,
+        crossover: Crossover,
+        basic_prob: float,
+        trust_prob: float,
+        cost_prob: float,
+        max_switched_genes: int,
         termination_criterion: TerminationCriterion = store.default_termination_criteria,
         population_generator: Generator = store.default_generator,
         population_evaluator: Evaluator = store.default_evaluator,
     ):
         super().__init__(problem, population_size, offspring_population_size)
 
+        self.BASIC_PROB = basic_prob
+        self.TRUST_PROB = trust_prob
+        self.COST_PROB = cost_prob
+        self.MAX_SWITCHED_GENES = max_switched_genes
+
+        if sum([self.COST_PROB, self.TRUST_PROB, self.BASIC_PROB]) - 1.0 > 1e-10:
+            raise ValueError("All probabilities should sum up to 1.0")
+
+        self.crossover_operator = crossover
+
         self.interaction_probability = interaction_probability
+        self.selection_operator = selection
+        self.mutation_operator = mutation
 
         self.termination_criterion = termination_criterion
 
@@ -46,16 +83,26 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
 
         self.observable.register(termination_criterion)
 
+        self.interacting_pool_size = (
+            int(self.population_size * self.interaction_probability // 2) * 2
+        )
+
     def create_initial_solutions(self) -> List[S]:
-        return [
+        solutions = [
             self.population_generator.new(self.problem)
             for _ in range(self.population_size)
         ]
 
+        # set index and trust for each solution
+        for index, solution in enumerate(solutions):
+            solution.id = index
+            self.ranking[solution.id] = self.MAX_TRUST // 2
+
+        return solutions
+
     def step(self):
         interacting_population = self.selection(self.solutions)
-        mutated_population = self.mutation(interacting_population)
-        self.evaluate(mutated_population)
+        self.interaction(interacting_population)
 
     def selection(self, population: List[S]) -> List[S]:
         """
@@ -64,35 +111,73 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
         :param population: Entire population.
         :return: Selected solutions.
         """
-        return random.sample(
-            population, int(len(population) * self.interaction_probability * 2) // 2
-        )
+        interacting_population = []
 
-    def mutation(self, interacting_population: List[FloatSolution]) -> List[S]:
+        for _ in range(self.interacting_pool_size):
+            solution = self.selection_operator.execute(population)
+            interacting_population.append(solution)
+
+        return interacting_population
+
+    def interaction(self, interacting_population: List[FloatSolution]):
         """
         Perform interaction between two individuals (indexes [0,1], [2,3], ...).
         Change their genes.
+        Only first from the pair is changed.
 
         :param interacting_population: Even-length list of solutions.
-        :return: Mutated list of solutions.
+        :return: List of changed solutions.
         """
         length = len(interacting_population)
         if length % 2 != 0:
             raise ValueError("List is not even-length.")
 
-        for ind1, ind2 in zip(
-            interacting_population[: length // 2], interacting_population[length // 2 :]
-        ):
-            if ind1.objectives[0] < ind2.objectives[0]:
-                ind2.variables[: ind2.number_of_variables // 2] = ind1.variables[
-                    : ind1.number_of_variables // 2
-                ]
-            else:
-                ind1.variables[: ind1.number_of_variables // 2] = ind2.variables[
-                    : ind2.number_of_variables // 2
-                ]
+        for index in range(0, length, 2):
+            ind1 = interacting_population[index]
+            ind2 = interacting_population[index + 1]
 
-        return interacting_population
+            trust_probability = self.TRUST_PROB * (
+                self.ranking[ind2.id] / self.MAX_TRUST
+            )
+
+            # TODO How to compute the Cost probablity ?
+            if ind1.objectives[0] < ind2.objectives[0]:
+                cost_probability = 0
+            else:
+                cost_probability = self.COST_PROB
+
+            exchange_probability = (
+                self.BASIC_PROB + trust_probability + cost_probability
+            )
+
+            if random.uniform(0.0, 1.0) < exchange_probability:
+                old_evaluation = ind1.objectives[0]
+                old_variables = copy(ind1.variables)
+
+                genes_to_switch = int(
+                    (trust_probability / self.TRUST_PROB) * self.MAX_SWITCHED_GENES
+                )
+                for _ in range(genes_to_switch):
+                    gene_to_switch = random.randint(
+                        0, self.problem.number_of_variables - 1
+                    )
+                    ind1.variables[gene_to_switch] = ind2.variables[gene_to_switch]
+
+                self.mutation_operator.execute(ind1)
+
+                new_evaluation = self.evaluate([ind1])
+                self.evaluations += 1
+
+                if new_evaluation[0].objectives[0] < old_evaluation:
+                    self.ranking[ind2.id] += 1
+                    self.ranking[ind1.id] -= 1
+
+                else:
+                    self.ranking[ind2.id] -= 1
+                    self.ranking[ind1.id] += 1
+
+                    ind1.variables = old_variables
+                    ind1.objectives[0] = old_evaluation
 
     def evaluate(self, solution_list: List[S]) -> List[S]:
         """
@@ -113,6 +198,26 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
     ) -> List[S]:
         pass
 
+    def update_progress(self) -> None:
+        observable_data = self.get_observable_data()
+        self.observable.notify_all(**observable_data)
+
+    def get_observable_data(self) -> dict:
+        return {
+            "PROBLEM": self.problem,
+            "EVALUATIONS": self.evaluations,
+            "SOLUTIONS": self.get_result(),
+            "AVERAGE_SOLUTIONS": self.get_average_objective(),
+            "RANKING": self.ranking,
+        }
+
+    def get_average_objective(self):
+        avg = 0
+        for sol in self.solutions:
+            avg += sol.objectives[0]
+
+        return avg / len(self.solutions)
+
     def get_result(self) -> R:
         return sorted(self.solutions, key=lambda solution: solution.objectives[0])[0]
 
@@ -120,15 +225,153 @@ class SocioSSGA(EvolutionaryAlgorithm[S, R]):
         return "Socio-cognitive SSGA"
 
 
-if __name__ == "__main__":
-    problem = Sphere(50)
-    socio = SocioSSGA(
-        problem=problem,
-        population_size=100,
-        offspring_population_size=1,
-        interaction_probability=0.3,
-        termination_criterion=StoppingByEvaluations(1000),
-    )
-    socio.run()
+class PrintObjectivesObserver2(Observer):
+    def __init__(self, frequency: float = 1.0) -> None:
+        """Show the number of evaluations, the best fitness and computing time.
 
-    print(socio.get_result())
+        :param frequency: Display frequency."""
+        self.display_frequency = frequency
+        self.epoch = []
+        self.fitness = []
+        self.average_fitness = []
+
+    def update(self, *args, **kwargs):
+        evaluations = kwargs["EVALUATIONS"]
+        solutions = kwargs["SOLUTIONS"]
+        average_solutions = kwargs["AVERAGE_SOLUTIONS"]
+
+        if (evaluations % self.display_frequency) == 0 and solutions:
+            if isinstance(solutions, list):
+                fitness = solutions[0].objectives
+            else:
+                fitness = solutions.objectives[0]
+
+            self.epoch.append(evaluations)
+            self.fitness.append(fitness)
+            self.average_fitness.append(average_solutions)
+
+            LOGGER.info("Evaluations: {}. fitness: {}".format(evaluations, fitness))
+
+def save_to_pdf(problems, plots_per_problem, note):
+
+    dir_name = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+
+    os.mkdir(str(dir_name))
+
+    fig_nums = plt.get_fignums()  
+    figs = [plt.figure(n) for n in fig_nums]
+
+    for i in range (0, len(problems)): 
+
+        
+        filename =  str(dir_name) + "/" + problems[i].get_name() + str(problems[i].number_of_variables) + ".pdf"
+        p = PdfPages(filename)
+        p.attach_note(note, [0,0])
+        for j in range(0, plots_per_problem):
+
+            fig = figs[i * plots_per_problem + j]
+            fig.savefig(p, format='pdf') 
+
+        
+        p.close()  
+
+if __name__ == "__main__":
+
+    basic_probs = [0.1, 0.2, 0.3, 0.1]
+    trust_probs = [0.6, 0.7, 0.6, 0.5]
+    cost_probs = [0.3, 0.1, 0.1, 0.4]
+
+    population_size = 100
+    offspring_population_size = 1
+    interaction_probability = 0.5
+    mutation_probability = 0.1
+    crossover_probability = 0.9
+    evaluations = 10000
+    observer_freq = 10
+
+    #basic_probs = [0.1]
+    #trust_probs = [0.6]
+    #cost_probs = [0.3]
+
+    sizes = [50, 100, 200]
+    problems = []
+
+    for size in sizes:
+        problems.append(Rastrigin(size))
+        problems.append(Sphere(size))
+
+    plots_per_problem = 2 # how many charts should be printed for each problem
+    number_of_trials = 1  # number of tests per problem 
+
+    epoch = []
+    fitness = []
+    average_fitness = []
+
+    test_data = ("Population size: " + str(population_size) + "\n" +
+    "Offspring size: " + str(offspring_population_size) + "\n" +
+    "Interaction probability: " + str(interaction_probability) + "\n" +
+    "Mutation probability: " + str(mutation_probability) + "\n" +
+    "Crossover probability: " + str(crossover_probability) + "\n" +
+     "Evaluations: " + str(evaluations) )
+    
+    for problem in problems:
+
+        epoch = []
+        fitness = []
+        average_fitness = []
+
+        for i, data in enumerate(zip(basic_probs, trust_probs, cost_probs)):
+            
+            #for j in range(0, number_of_trials):
+
+            socio = SocioSSGA(
+                problem=problem,
+                population_size=population_size,
+                offspring_population_size=offspring_population_size,
+                interaction_probability=interaction_probability,
+                selection=RouletteWheelSelection(),
+                mutation=UniformMutation(mutation_probability),
+                crossover=SBXCrossover(crossover_probability),
+                basic_prob=data[0],
+                trust_prob=data[1],
+                cost_prob=data[2],
+                max_switched_genes=int(problem.number_of_variables * 0.75),
+                termination_criterion=StoppingByEvaluations(evaluations),
+            )
+
+            observer = PrintObjectivesObserver2(observer_freq)
+            socio.observable.register(observer)
+
+            socio.run()
+            print("Problem: " + problem.get_name() + " " + str(problem.number_of_variables))
+            print(len(observer.epoch))
+
+            epoch.append(observer.epoch)
+            fitness.append(observer.fitness)
+            average_fitness.append(observer.average_fitness)
+
+            #epoch = [x + y for x, y in zip(epoch, observer.epoch)] 
+            #fitness = [x + y for x, y in zip(epoch, observer.fitness)]
+            #average_fitness = [x + y for x, y in zip(epoch, observer.average_fitness)] 
+
+        plt.figure()
+        plt.xlabel("Ewaluacje")
+        plt.ylabel("Fitness")
+        plt.title("Comparison of different probabilities (fitness vs average fitness)")
+        for i, data2 in enumerate(zip(epoch, fitness, average_fitness)):
+            plt.plot(data2[0], data2[1], label="fitness " + str(i))
+            plt.plot(data2[0], data2[2], label="average_fitness " + str(i))
+        plt.legend()
+        #plt.show()
+
+        plt.figure()
+        plt.xlabel("Ewaluacje")
+        plt.ylabel("Fitness")
+        plt.title("Comparison of different probabilities (fitness)")
+        for i, data2 in enumerate(zip(epoch, fitness, basic_probs)):
+            plt.plot(data2[0], data2[1], label="Basic prob: " + str(data2[2]))
+        plt.legend()
+        #plt.show()
+
+    save_to_pdf(problems, plots_per_problem, test_data)
+
